4cff92e47d8aa74ab5876dc0f1f8d02e93ef5bc9
pradeepSharmadev <amarahipradeep7319@gmail.com>
2025-06-19 20:13:35 +0530

    Backend server for collabrative application is created with http server and websocket server

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const webSocket = require('ws');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const wss = new webSocket.Server({ server });
+
+//document store
+const documents = "";
+
+wss.on("connection", (ws)=>{
+    console.log("New Connection Connected")
+
+    ws.send(JSON.stringify({type: "init", data: document}));
+    ws.on("message", (message)=>{
+        try {
+            const parseMessage = JSON.parse(message);
+            if (parseMessage.type === "update") {
+                document = parseMessage.data;
+
+                //broadcast the updated document to all connected clients
+                wss.clients.forEach((client) => {
+                    if (client.readyState === webSocket.OPEN) {
+                        client.send(JSON.stringify({type: "update", data: document}));
+                    }
+                })
+            }
+        } catch (error) {
+            console.log(error, "Error parsing message")
+        }
+    })
+})
