8654e833de20e8d85d797bda81a73164b11ea8cb
pradeepSharmadev <amarahipradeep7319@gmail.com>
2025-06-21 18:58:37 +0530

    Implement WebSocket connection for collaborative document editing and update package dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [document, setDocument] = useState("");
+  const [socket, setSocket] = useState(null);
 
+  useEffect(() => {
+    const newSocket = new WebSocket("ws://localhost:5000/ws");
+    setSocket(newSocket);
+
+    newSocket.onopen = () => {
+      console.log("Web Socket connection Establish");
+    };
+
+    newSocket.onmessage = (message) => {
+      try {
+        const parseMessage = JSON.parse(message.data);
+        if (parseMessage.type === "init") {
+          setDocument(parseMessage.data);
+        } else if (message.type === "update") {
+          setDocument(parseMessage.data);
+        }
+      } catch (error) {
+        console.error(error, "Error parsing message");
+      }
+    };
+
+    newSocket.close = () => {
+      console.log("Web Socket connection closed");
+    };
+    newSocket.onerror = (error) => {
+      console.log("WebSocket Error", error);
+    };
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
+  const handleChange = (e) => {
+    const newDocument = e.target.value;
+    setDocument(newDocument);
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(
+        JSON.stringify({
+          type: "update",
+          data: newDocument,
+        })
+      );
+    }
+  };
   return (
-    
-  )
-}
+    <>
+      <div>
+        <h1>Collaborative Editor</h1>
+        <div>
+          <textarea
+            name="editor"
+            id="editor"
+            cols="30"
+            rows="10"
+            value={document}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const http = require('http');
-const webSocket = require('ws');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const webSocket = require("ws");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
@@ -10,27 +10,30 @@ const server = http.createServer(app);
 const wss = new webSocket.Server({ server });
 
 //document store
-const documents = "";
+let document = "";
 
-wss.on("connection", (ws)=>{
-    console.log("New Connection Connected")
+wss.on("connection", (ws) => {
+  console.log("New Connection Connected");
 
-    ws.send(JSON.stringify({type: "init", data: document}));
-    ws.on("message", (message)=>{
-        try {
-            const parseMessage = JSON.parse(message);
-            if (parseMessage.type === "update") {
-                document = parseMessage.data;
+  ws.send(JSON.stringify({ type: "init", data: document }));
+  ws.on("message", (message) => {
+    try {
+      const parseMessage = JSON.parse(message);
+      if (parseMessage.type === "update") {
+        document = parseMessage.data;
 
-                //broadcast the updated document to all connected clients
-                wss.clients.forEach((client) => {
-                    if (client.readyState === webSocket.OPEN) {
-                        client.send(JSON.stringify({type: "update", data: document}));
-                    }
-                })
-            }
-        } catch (error) {
-            console.log(error, "Error parsing message")
-        }
-    })
-})
+        //broadcast the updated document to all connected clients
+        wss.clients.forEach((client) => {
+          if (client.readyState === webSocket.OPEN) {
+            client.send(JSON.stringify({ type: "update", data: document }));
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error, "Error parsing message");
+    }
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
